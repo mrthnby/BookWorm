56e6ffbd3c373502c994781bbc1840128a6964ea
mrthnby <meralihanbay@gmail.com>
2024-03-05 20:11:01 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Mert Ali Hanbay on 5.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
